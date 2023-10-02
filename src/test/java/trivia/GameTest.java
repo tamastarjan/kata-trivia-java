f7c0dfddbf7baf0f5472c5eab74b48ee538e5489
Tamas Tarjan <tamas.tarjan@productdock.com>
2023-10-02 14:45:07 +0200

    Auto format

--- src/test/java/trivia/GameTest.java
+++ src/test/java/trivia/GameTest.java
@@ -1,4 +1,3 @@
-
 package trivia;
 
 import org.junit.Ignore;
@@ -11,56 +10,58 @@ import java.util.Random;
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
-	@Test
-	public void caracterizationTest() {
-		// runs 10.000 "random" games to see the output of old and new code mathces
-		for (int seed = 1; seed < 10_000; seed++) {
-			testSeed(seed, false);
-		}
-	}
+  @Test
+  public void caracterizationTest() {
+    // runs 10.000 "random" games to see the output of old and new code mathces
+    for (int seed = 1; seed < 10_000; seed++) {
+      testSeed(seed, false);
+    }
+  }
 
-	private void testSeed(int seed, boolean printExpected) {
-		String expectedOutput = extractOutput(new Random(seed), new Game());
-		if (printExpected) {
-			System.out.println(expectedOutput);
-		}
-		String actualOutput = extractOutput(new Random(seed), new GameBetter());
-		assertEquals("Change detected for seed " + seed +
-						 ". To breakpoint through it, run this seed alone using the (ignored) test below",
-			expectedOutput, actualOutput);
-	}
-	@Test
-	@Ignore("enable back and set a particular seed to see the output")
-	public void oneSeed() {
-		testSeed(1, true);
-	}
+  private void testSeed(int seed, boolean printExpected) {
+    String expectedOutput = extractOutput(new Random(seed), new Game());
+    if (printExpected) {
+      System.out.println(expectedOutput);
+    }
+    String actualOutput = extractOutput(new Random(seed), new GameBetter());
+    assertEquals("Change detected for seed " + seed +
+            ". To breakpoint through it, run this seed alone using the (ignored) test below",
+        expectedOutput, actualOutput);
+  }
 
-	private String extractOutput(Random rand, IGame aGame) {
-		PrintStream old = System.out;
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try (PrintStream inmemory = new PrintStream(baos)) {
-			// WARNING: System.out.println() doesn't work in this try {} as the sysout is captured and recorded in memory.
-			System.setOut(inmemory);
+  @Test
+  @Ignore("enable back and set a particular seed to see the output")
+  public void oneSeed() {
+    testSeed(1, true);
+  }
 
-			aGame.add("Chet");
-			aGame.add("Pat");
-			aGame.add("Sue");
+  private String extractOutput(Random rand, IGame aGame) {
+    PrintStream old = System.out;
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    try (PrintStream inmemory = new PrintStream(baos)) {
+      // WARNING: System.out.println() doesn't work in this try {} as the sysout is captured and recorded in
+      // memory.
+      System.setOut(inmemory);
 
-			boolean notAWinner = false;
-			do {
-				aGame.roll(rand.nextInt(5) + 1);
+      aGame.add("Chet");
+      aGame.add("Pat");
+      aGame.add("Sue");
 
-				if (rand.nextInt(9) == 7) {
-					notAWinner = aGame.wrongAnswer();
-				} else {
-					notAWinner = aGame.wasCorrectlyAnswered();
-				}
+      boolean notAWinner = false;
+      do {
+        aGame.roll(rand.nextInt(5) + 1);
 
-			} while (notAWinner);
-		} finally {
-			System.setOut(old);
-		}
+        if (rand.nextInt(9) == 7) {
+          notAWinner = aGame.wrongAnswer();
+        } else {
+          notAWinner = aGame.wasCorrectlyAnswered();
+        }
 
-		return new String(baos.toByteArray());
-	}
+      } while (notAWinner);
+    } finally {
+      System.setOut(old);
+    }
+
+    return new String(baos.toByteArray());
+  }
 }
